64a538d840ce41d4c9d447c9cca315a3dd00f090
Ruslan Ragilo <ruslanragilo7@gmail.com>
2022-04-01 23:25:41 +0300

    edit function filter popular

--- js/app.js
+++ js/app.js
@@ -1,33 +1,29 @@
 //variables and filter films top
 
 //filter films top
-let totalFilms = {
-  top250: 'TOP_250_BEST_FILMS&page=1',
-  top100: 'TOP_100_POPULAR_FILMS&page=1'
-};
+let totalFilms = ["TOP_250_BEST_FILMS&page=1", "TOP_100_POPULAR_FILMS&page=1"];
 
 const itemFilterFilms = document.querySelectorAll(".item_filter_films");
 
-for(let itemFilterFilm of itemFilterFilms) {
+let totalNumberPagination;
+
+for (let itemFilterFilm of itemFilterFilms) {
   if (itemFilterFilm.checked) {
-    filmCount = totalFilms['top' + itemFilterFilm.value];
+    totalNumberPagination = Number(itemFilterFilm.value);
   }
 }
 
+let API_URL = `https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=${totalFilms[0]}`;
 
-let API_URL =
-  `https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=`;
-
-  wrpapperFilms = document.querySelector(".wrpapper_films"),
-  headerSearch = document.querySelector(".header__search"),
-  pagination = document.querySelector(".pagination");
+(wrpapperFilms = document.querySelector(".wrpapper_films")),
+  (headerSearch = document.querySelector(".header__search")),
+  (pagination = document.querySelector(".pagination"));
 
 let filmsEl;
 let responseFilms;
 let responseKey;
 
 const apiKeyURL = "http://localhost:3000/key";
-
 
 //fuction show box film
 const showBoxFilm = (movie) => {
@@ -36,16 +32,22 @@ const showBoxFilm = (movie) => {
   filmsEl.innerHTML = `
   <div class="wrapper_film">
       <div class="movie__cover-inner">
-          <img src="${movie.posterUrlPreview}" class="movie__cover" alt="${movie.nameRu}">
+          <img src="${movie.posterUrlPreview}" class="movie__cover" alt="${
+    movie.nameRu
+  }">
           <div class="movie__cover--darkened"></div>
       </div>
       <div class="movie__info">
           <div class="movie__title">${movie.nameRu}</div>
-          <div class="movie__average movie__average--green">${movie.rating !== null ? String(movie.rating.substring(0,1)) : 7}</div>
+          <div class="movie__average movie__average--green">${
+            movie.rating !== null ? String(movie.rating.substring(0, 1)) : 7
+          }</div>
       </div>
   <div/>`;
   wrpapperFilms.appendChild(filmsEl);
 };
+
+
 
 //fetch request
 const fetchMovies = async (url, key) => {
@@ -72,6 +74,8 @@ const getMovies = async (apiKeyURL) => {
   fetchMovies(API_URL, responseKey);
 };
 
+
+
 getMovies(apiKeyURL);
 
 //function for show films
@@ -82,58 +86,62 @@ const showMovies = (data) => {
 };
 
 //add pagination number
-let totalNumberPagination;
-const addPaginationNumber = () => {
+var numberPaginations;
+const addPaginationNumber = (totalNumberPagination) => {
   for (let i = 1; i <= totalNumberPagination / 20; i++) {
-    pagination.innerHTML += `<p class="number_pagination ${i == 1 ? ' active': + ''}">${i}</p>`
+    pagination.innerHTML += `<p class="number_pagination ${
+      i == 1 ? " active" : +""
+    }">${i}</p>`;
   }
+  numberPaginations = document.querySelectorAll(".number_pagination");
 };
 
+addPaginationNumber(totalNumberPagination);
 
-addPaginationNumber();
-
-//fuction for pagination
-const numberPaginations = document.querySelectorAll(".number_pagination");
-for (let numberPagination of numberPaginations) {
+//function for pagination
+let paginations = () => {for (let numberPagination of numberPaginations) {
   numberPagination.addEventListener("click", (e) => {
-  for (let numberPagination of numberPaginations){
-    numberPagination.classList.remove("active")
-  };
-  if (e.target === numberPagination) {
+    for (let numberPagination of numberPaginations) {
+      numberPagination.classList.remove("active");
+    }
+    if (e.target === numberPagination) {
       e.target.classList.add("active");
       wrpapperFilms.innerHTML = "";
       fetchMovies(
-        API_URL.substring(0, API_URL.length - 1) +
-          e.target.textContent,
+        API_URL.substring(0, API_URL.length - 1) + e.target.textContent,
         responseKey
       );
     }
-  })
-};
+  });
+}}
+
+paginations();
+
+
+//function filter movies popular
+itemFilterFilms.forEach((itemFilterFilm, index) => {
+  itemFilterFilm.addEventListener("change", (e) => {
+    wrpapperFilms.innerHTML = "";
+    filmFilter = totalFilms[index];
+    fetchMovies(API_URL.substring(0, 60) + filmFilter, responseKey);
+    totalNumberPagination = Number(e.target.value);
+    pagination.innerHTML = "";
+    addPaginationNumber(totalNumberPagination);
+    paginations();
+  });
+});
+
+
 
 //function for search
 headerSearch.addEventListener("input", (e) => {
-  e.target.value.length > 0 ? pagination.style.visibility = "hidden": pagination.style.visibility = "visible";
+  e.target.value.length > 0
+    ? (pagination.style.visibility = "hidden")
+    : (pagination.style.visibility = "visible");
   wrpapperFilms.innerHTML = "";
-    responseFilms.filter((movie, i) => {
-      if (movie.nameRu.toLowerCase().indexOf(e.target.value.toLowerCase()) + 1) {
-        showBoxFilm(movie);
-      }
-    });
+  responseFilms.filter((movie, i) => {
+    if (movie.nameRu.toLowerCase().indexOf(e.target.value.toLowerCase()) + 1) {
+      showBoxFilm(movie);
+    }
+  });
 });
-
-//function filter movies popular
-itemFilterFilms.forEach(itemFilterFilm => {
-  itemFilterFilm.addEventListener('change', (e) => {
-    wrpapperFilms.innerHTML = "";
-    fetchMovies(
-      API_URL+totalFilms['top' + e.target.value],
-      responseKey
-    );
-    totalNumberPagination = Number(e.target.value);
-    pagination.innerHTML = '';
-    addPaginationNumber();
-  })
-})
-
-
